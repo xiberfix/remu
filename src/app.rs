0eefecd9875b35d3e805cb84493ee68229c64548
Michael Khryashchev <me@xiberfix.dev>
2023-05-22 18:53:42 +0400

    Add memory loader

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,20 @@
 use eframe::egui;
 
 pub struct App {
-    text: String,
-    number: u32,
+    path_text: String,
+    address_text: String,
+    error: Option<String>,
+    memory: Vec<u8>,
 }
 
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            text: "Hello, World!".to_owned(),
-            number: 42,
+            path_text: "".to_owned(),
+            address_text: "".to_owned(),
+            error: None,
+            memory: vec![0; 0x10000], // 64 KiB
         }
     }
 }
@@ -18,19 +22,69 @@ impl Default for App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { text, number } = self;
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My Application");
-            ui.horizontal(|ui| {
-                let label = ui.label("text: ");
-                ui.text_edit_singleline(text).labelled_by(label.id);
-            });
-            ui.add(egui::Slider::new(number, 0..=100).text("number"));
-            if ui.button("increment").clicked() {
-                *number += 1;
-            }
-            ui.label(format!("text={}; number={}", text, number));
+        egui::CentralPanel::default().show(ctx, |_ui| {
+            egui::Window::new("Loader")
+                .resizable(true)
+                .default_width(300.0)
+                .show(ctx, |ui| {
+                    self.loader_view(ui);
+                });
         });
+    }
+}
+
+
+impl App {
+    fn loader_view(&mut self, ui: &mut egui::Ui) {
+        egui::Grid::new("loader")
+            .num_columns(2)
+            .spacing([24.0, 8.0])
+            .striped(true)
+            .show(ui, |ui| {
+                let Self { path_text: file, address_text: address, .. } = self;
+
+                ui.label("File:");
+                ui.add(egui::TextEdit::singleline(file).hint_text("path"));
+                ui.end_row();
+
+                ui.label("Address:");
+                ui.add(egui::TextEdit::singleline(address).hint_text("xxxx"));
+                ui.end_row();
+
+                if ui.button("Load").clicked() {
+                    self.load();
+                };
+
+                if let Some(error) = &self.error {
+                    ui.colored_label(egui::Color32::RED, error);
+                }
+                ui.end_row();
+            });
+    }
+
+
+    fn load_data(&mut self) -> Result<(), String> {
+        // parse address
+        let begin = usize::from_str_radix(&self.address_text.to_owned(), 16)
+            .map_err(|e| e.to_string())?;
+
+        // read file
+        let data = std::fs::read(&self.path_text)
+            .map_err(|e| e.to_string())?;
+
+        // copy data into memory
+        let end = begin + data.len();
+        let len = self.memory.len();
+        if end > len {
+            return Err("address out of range".to_owned());
+        }
+        self.memory[begin..end].copy_from_slice(&data);
+
+        Ok(())
+    }
+
+
+    fn load(&mut self) {
+        self.error = self.load_data().err();
     }
 }
