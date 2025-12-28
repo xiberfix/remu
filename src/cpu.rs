088e1b0570a2fe4b6758c3ba26396a7f75824228
Michael Khryashchev <me@xiberfix.dev>
2025-12-28 17:23:10 +0400

    add hook for CP/M basic functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -189,29 +189,29 @@ impl Cpu {
         )
     }
 
-    fn bc(&self) -> u16 {
+    pub fn bc(&self) -> u16 {
         ((self.b as u16) << 8) | (self.c as u16)
     }
 
-    fn set_bc(&mut self, value: u16) {
+    pub fn set_bc(&mut self, value: u16) {
         self.b = (value >> 8) as u8;
         self.c = (value & 0xFF) as u8;
     }
 
-    fn de(&self) -> u16 {
+    pub fn de(&self) -> u16 {
         ((self.d as u16) << 8) | (self.e as u16)
     }
 
-    fn set_de(&mut self, value: u16) {
+    pub fn set_de(&mut self, value: u16) {
         self.d = (value >> 8) as u8;
         self.e = (value & 0xFF) as u8;
     }
 
-    fn hl(&self) -> u16 {
+    pub fn hl(&self) -> u16 {
         ((self.h as u16) << 8) | (self.l as u16)
     }
 
-    fn set_hl(&mut self, value: u16) {
+    pub fn set_hl(&mut self, value: u16) {
         self.h = (value >> 8) as u8;
         self.l = (value & 0xFF) as u8;
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::bus::Bus;
 use crate::cpu::Cycles;
 
 mod bus;
@@ -5,28 +6,71 @@ mod cpu;
 mod machine;
 
 fn main() {
-    let mut machine = machine::SimpleMachine::new();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: {} <test>", args[0]);
+        std::process::exit(1);
+    }
 
-    // 0000: JP 0003
-    // 0003: NOP
-    // 0004: NOP
-    // 0005: HALT
-    #[rustfmt::skip]
-    let program: [u8; _] = [
-        0xC3, 0x03, 0x00,
-        0x00,
-        0x00,
-        0x76,
-    ];
-
-    machine.load(0x0000, &program);
-    run_machine(&mut machine, 20);
+    let path = &args[1];
+    println!("test: {}", path);
+    let cycles = run_test(path);
+    println!("\ncycles: {}", cycles);
 }
 
-fn run_machine(machine: &mut machine::SimpleMachine, max_cycles: Cycles) {
+fn run_test(path: &str) -> Cycles {
+    let mut machine = machine::SimpleMachine::new();
     let mut cycles: Cycles = 0;
-    while cycles < max_cycles {
+
+    let program = std::fs::read(path).expect("failed to read test file");
+    machine.load(0x0100, &program);
+    machine.cpu.pc = 0x0100;
+
+    // CP/M hook
+    machine.bus.write(0x0005, 0xC9); // RET
+    machine.bus.write(0x0000, 0x76); // HALT
+
+    loop {
+        if machine.cpu.state == cpu::State::Halted {
+            break;
+        }
+
+        // CP/M hook
+        if machine.cpu.pc == 0x0005 {
+            process_cpm_call(&mut machine);
+        }
+
         cycles += machine.step();
-        println!("{}", machine.cpu.to_string());
+    }
+
+    cycles
+}
+
+fn process_cpm_call(machine: &mut machine::SimpleMachine) {
+    match machine.cpu.c {
+        0x00 => {
+            // system reset
+            machine.cpu.pc = 0x0000;
+        }
+        0x02 => {
+            // character output
+            let char = machine.cpu.e;
+            print!("{}", char as char);
+        }
+        0x09 => {
+            // string output
+            let mut addr = machine.cpu.de();
+            loop {
+                let char = machine.bus.read(addr);
+                if char == b'$' {
+                    break;
+                }
+                print!("{}", char as char);
+                addr = addr.wrapping_add(1);
+            }
+        }
+        _ => {
+            println!("unsupported {:02X} CP/M call", machine.cpu.c);
+        }
     }
 }
